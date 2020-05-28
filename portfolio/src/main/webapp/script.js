112aa71db22e3bb1af801ccb699b0bdeda54bff3
Zachary Tan <tttzach@gmail.com>
2020-05-28 18:24:45 -0400

    Tweak formatting to follow style guides

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,10 +14,9 @@
 
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName) {
-  var i, x;
-  x = document.getElementsByClassName("containerTab");
+  containerTab = document.getElementsByClassName("containerTab");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    containerTab[i].style.display = "none";
   }
   document.getElementById(tabName).style.display = "inline-block";
 }
