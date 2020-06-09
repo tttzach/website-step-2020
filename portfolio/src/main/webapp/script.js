2cfa916b6eb6f5bf6d593aa118c714487a667285
tttzach <tanzachary@google.com>
2020-06-09 12:39:44 -0400

    Add basic functioning Google Chart to About Me tab

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -147,3 +147,28 @@ function createMapInfoWindow(map, marker) {
     infoWindow.open(map, marker);
   });
 }
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Lions', 10],
+          ['Tigers', 5],
+          ['Bears', 15]
+        ]);
+
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
