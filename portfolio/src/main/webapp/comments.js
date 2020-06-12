5161a4e8413dfef699634bb2a9179e91ab8c4f6a
tttzach <tanzachary@google.com>
2020-06-12 17:44:05 -0400

    Break script.js into multiple .js files according to functionality

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -12,52 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
-function openTab(tabName) {
-  const containerTabs = document.getElementsByClassName("containerTab");
-  for (var containerTab of containerTabs) {
-    containerTab.style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "inline-block";
-}
-
-// Next/previous controls for slideshow under personal tab
-var plusSlides = (function (offset) {
-  var slideIndex = 1
-  return function(offset) {
-    slideIndex += offset;
-    const slides = document.getElementsByClassName("slide");
-    if (slideIndex > slides.length) {slideIndex = 1}
-    if (slideIndex < 1) {slideIndex = slides.length}
-    for (var slide of slides) {
-      slide.style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-    return slideIndex;
-  }
-})();
-
-// Fetches a hard-coded JSON string from the server and adds it as a greeting to the DOM
-async function getGreeting() {
-  const response = await fetch('/data');
-  const json = await response.json();
-  const greeting = jsonToHtml(json);
-  document.getElementById('greeting-container').innerHTML = greeting;
-}
-
-function jsonToHtml(podmates) {
-  var html = "<h1>Hello ";
-  const lastIndex = podmates.length - 1;
-  for (const [index, podmate] of podmates.entries()) {
-    html += index == lastIndex ? " and "
-            : index != 0 ? ", "
-            : "";
-    html += podmate;
-  }
-  html += "!</h1>";
-  return html;
-}
-
 // Fetches comments from the server and adds it to the DOM
 function getCommentForm() {
   fetch('/comment-form')
