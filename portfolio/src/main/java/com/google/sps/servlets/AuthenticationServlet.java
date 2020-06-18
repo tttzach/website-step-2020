3c02420b69351af6bb4c4e54be01ed955da95f77
tttzach <tanzachary@google.com>
2020-06-18 15:26:05 -0400

    Sort imports

--- portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AuthenticationServlet.java
@@ -14,15 +14,15 @@
 
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
-import org.javatuples.Pair;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.javatuples.Pair;
 
 @WebServlet("/authentication")
 public class AuthenticationServlet extends HttpServlet {
